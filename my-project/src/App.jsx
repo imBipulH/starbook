3ebe291af1396d87a0c3c48cf7eea1a5c235b652
Bipul Hajong <126290681+imBipulH@users.noreply.github.com>
2023-12-14 09:56:46 +0000

    feat: Add Registration page
    Added a new Registration page component and imported it in App.jsx.

--- my-project/src/App.jsx
+++ my-project/src/App.jsx
@@ -1,9 +1,11 @@
 import Login from "./pages/Login/Login";
+import Registration from "./pages/Registration/Registration";
 
 function App() {
   return (
     <>
       <Login />
+      <Registration />
     </>
   );
 }

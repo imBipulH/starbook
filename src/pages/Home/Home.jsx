ce38621b0ddb19e8160db90257914c3f7f82ce18
imBipulH <bipulh62@gmail.com>
2023-12-18 16:35:00 +0600

    12/18 coverPhoto failed

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -16,35 +16,39 @@ const Home = () => {
   const [singlePost, setSinglePost] = useState([]);
   const data = useSelector((state) => state.userLoginInfo.userInfo);
   const db = getDatabase();
+
   onAuthStateChanged(auth, (user) => {
     console.log(user, "user");
-    if (user && user.emailVerified) {
+    if (user.emailVerified) {
       setVerify(true);
-      //  dispatch(userLoginInfo(user));
+      dispatch(userLoginInfo(user));
       localStorage.setItem("userLoginInfo", JSON.stringify(user));
     }
   });
 
   const handlePost = () => {
-    set(push(ref(db, "singlePost")), {
-      post: post,
-      postSenderId: data.uid,
-      postSenderName: data.displayName,
-      date: `${new Date().getFullYear()} - ${
-        new Date().getMonth() + 1
-      } - ${new Date().getDate()}, ${new Date().getHours()}:${new Date().getMinutes()}`,
-    });
-  };
-  useEffect(() => {
-    const singlePostRef = ref(db, "singlePost/");
-    onValue(singlePostRef, (snapshot) => {
-      let arr = [];
-      snapshot.forEach((item) => {
-        arr.push(item.val());
-        setSinglePost((prevMessages) => [...prevMessages, ...arr]);
+    if (post.length > 0) {
+      set(push(ref(db, "singlePost")), {
+        post: post,
+        postSenderId: data.uid,
+        postSenderName: data.displayName,
+        date: `${new Date().getFullYear()} - ${
+          new Date().getMonth() + 1
+        } - ${new Date().getDate()}, ${new Date().getHours()}:${new Date().getMinutes()}`,
       });
+    }
+  };
+
+  /*  useEffect(() => {
+    const singlePostRef = ref(db, "singlePost");
+    onValue(singlePostRef, (sanpshot) => {
+      let arr = [];
+      sanpshot.forEach((item) => {
+        arr.push(item.val());
+      });
+      setSinglePost(arr);
     });
-  }, []);
+  }, []); */
 
   return (
     <>
@@ -62,85 +66,81 @@ const Home = () => {
           </div>
         </div>
       ) : (
-        singlePost.map((item, index) => (
-          <div key={index} className="bg-primary w-full h-screen">
-            <Navbar page="Home" />
-            <div className="w-[980px]  m-auto">
-              <div className="flex justify-start items-center gap-4">
-                <img
-                  className="w-12 h-12"
-                  src="https://cdn-icons-png.flaticon.com/512/147/147144.png"
+        <div className="bg-primary w-full h-screen">
+          <Navbar page="Home" />
+          <div className="w-[980px]  m-auto">
+            <div className="flex justify-start items-center gap-4">
+              <img className="w-12 h-12 rounded-full" src={data.photoURL} />
+
+              <div className="text-white">
+                <p>{data.displayName && data.displayName}</p>
+                <p className="text-sm">{data.email && data.email}</p>
+              </div>
+            </div>
+            <div className="text-white  w-full">
+              <div className="w-full px-8">
+                <input
+                  onChange={(e) => setPost(e.target.value)}
+                  value={post}
+                  className="w-full box-border my-6 pl-8 py-2 rounded-md border border-gray-500 bg-transparent"
+                  type="text"
+                  placeholder="Write a post"
                 />
-
-                <div className="text-white">
-                  <p>{data.displayName && data.displayName}</p>
-                  <p className="text-sm">{data.email && data.email}</p>
-                </div>
-              </div>
-              <div className="text-white  w-full">
-                <div className="w-full px-8">
-                  <input
-                    onChange={(e) => setPost(e.target.value)}
-                    className="w-full box-border my-6 pl-8 py-2 rounded-md border border-gray-500 bg-transparent"
-                    type="text"
-                    placeholder="Write a post"
-                  />
-                </div>
-
-                <div className="flex justify-between items-center pl-2 pr-8 w-full border-b border-gray-500 pb-8 bg-transparent ">
-                  <img
-                    className="w-8 h-8 rounded-full"
-                    src="https://cdn-icons-png.flaticon.com/512/147/147144.png"
-                  />
-                  <div className="bg-gray-500 w-full h-[1px]"></div>
-                  <button
-                    onClick={handlePost}
-                    className="px-2 py-1 text-sm bg-gray-500 text-white rounded relative "
-                  >
-                    Post
-                  </button>
-                </div>
               </div>
 
-              <div>
-                <div className="text-white flex items-center gap-4 my-4">
-                  <img
-                    src="https://cdn-icons-png.flaticon.com/512/147/147144.png"
-                    alt="DP"
-                    className="w-12 h-12 rounded-full"
-                  />
-                  <div>
-                    <p className="text-xl text-white font-semibold">
-                      Bipul Hajong
-                    </p>
-                    <div className="flex items-center mt-1 gap-2">
-                      <p className="text-sm">8 hours ago</p>
-                      <MdPublic className="text-sm" />
+              <div className="flex justify-between items-center pl-2 pr-8 w-full border-b border-gray-500 pb-8 bg-transparent ">
+                <img className="w-8 h-8 rounded-full" src={data.photoURL} />
+                <div className="bg-gray-500 w-full h-[1px]"></div>
+                <button
+                  onClick={handlePost}
+                  className="px-2 py-1 text-sm bg-gray-500 text-white rounded relative "
+                >
+                  Post
+                </button>
+              </div>
+            </div>
+
+            {/* Start of timeline */}
+
+            {singlePost &&
+              singlePost.map((item, id) => (
+                <div key={id}>
+                  <div className="text-white flex items-center gap-4 my-4">
+                    <img
+                      src="https://cdn-icons-png.flaticon.com/512/147/147144.png"
+                      alt="DP"
+                      className="w-12 h-12 rounded-full"
+                    />
+                    <div>
+                      <p className="text-xl text-white font-semibold">
+                        Bipul Hajong
+                      </p>
+                      <div className="flex items-center mt-1 gap-2">
+                        <p className="text-sm">8 hours ago</p>
+                        <MdPublic className="text-sm" />
+                      </div>
+                    </div>
+                  </div>
+                  <div className="w-full px-12 border-b border-gray-500 pb-4">
+                    <p className="text-white text-xl">{item.post}</p>
+
+                    <div className="text-white flex items-center gap-8 mt-8">
+                      <p>
+                        Like <span>2</span>
+                      </p>
+                      <p>
+                        Comment <span>2</span>
+                      </p>
+                      <p>
+                        Share <span>2</span>
+                      </p>
                     </div>
                   </div>
                 </div>
-                <div className="w-full px-12 border-b border-gray-500 pb-4">
-                  <p className="text-white text-xl">
-                    Hi, this is Bipul Hajong. Having dream to become a full
-                    stack developer.
-                  </p>
-
-                  <div className="text-white flex items-center gap-8 mt-8">
-                    <p>
-                      Like <span>2</span>
-                    </p>
-                    <p>
-                      Comment <span>2</span>
-                    </p>
-                    <p>
-                      Share <span>2</span>
-                    </p>
-                  </div>
-                </div>
-              </div>
-            </div>
+              ))}
+            {/* End of timeline */}
           </div>
-        ))
+        </div>
       )}
     </>
   );
--- src/pages/Profile/Profile.jsx
+++ src/pages/Profile/Profile.jsx
@@ -1,12 +1,31 @@
 import { useSelector } from "react-redux";
 import Navbar from "../../components/Navbar.jsx/Navbar";
 import { BiUpload } from "react-icons/bi";
-import { useState } from "react";
+import { useState, createRef } from "react";
+import Cropper from "react-cropper";
+import "cropperjs/dist/cropper.css";
+import {
+  getDownloadURL,
+  getStorage,
+  ref,
+  uploadString,
+} from "firebase/storage";
+import { getDatabase, ref as dref, update } from "firebase/database";
+import { getAuth, updateProfile } from "firebase/auth";
 
 const Profile = () => {
   const data = useSelector((state) => state.userLoginInfo.userInfo);
+  console.log(data);
   const [profileModal, setProfileModal] = useState(false);
+  const [coverModal, setCoverModal] = useState(false);
   const [image, setImage] = useState("");
+  const [cover, setCover] = useState("");
+  const [profilePhoto, setProfilePhoto] = useState();
+  const [coverPhoto, setCoverPhoto] = useState();
+  const auth = getAuth();
+  const [cropData, setCropData] = useState("#");
+  const cropperRef = createRef();
+  const db = getDatabase();
 
   const SideList = ({ title, description }) => {
     return (
@@ -19,15 +38,57 @@ const Profile = () => {
     );
   };
 
+  const getCropData = () => {
+    if (typeof cropperRef.current?.cropper !== "undefined") {
+      setCropData(cropperRef.current?.cropper.getCroppedCanvas().toDataURL());
+      const message4 = cropData;
+      const storage = getStorage();
+      const storageRef = ref(storage, data.uid);
+      uploadString(storageRef, message4, "data_url").then(() => {
+        getDownloadURL(storageRef).then((downloadURL) => {
+          setProfilePhoto(downloadURL);
+          // setProfileModal(false);
+          updateProfile(auth.currentUser, {
+            photoURL: downloadURL,
+          }).then(() => {
+            update(dref(db, "users/" + data.uid), {
+              dp: downloadURL,
+            });
+            setProfileModal(false);
+          });
+        });
+      });
+    }
+  };
+  const getCoverData = () => {
+    if (typeof cropperRef.current?.cropper !== "undefined") {
+      setCropData(cropperRef.current?.cropper.getCroppedCanvas().toDataURL());
+      const message4 = cropData;
+      const storage = getStorage();
+      const storageRef = ref(storage, data.uid);
+      uploadString(storageRef, message4, "Cover_Photo").then(() => {
+        getDownloadURL(storageRef).then((downloadURL) => {
+          setCoverPhoto(downloadURL); /* .then(() => {
+          // setProfileModal(false);
+          /*    updateProfile(auth.currentUser, {
+            photoURL: downloadURL,
+          }) */ /*
+            update(dref(db, "users/" + data.uid), {
+              coverPhoto: downloadURL,
+            });
+            setCoverModal(false);
+          }); */
+        });
+      });
+    }
+  };
+
   const imageUpload = (e) => {
     e.preventDefault();
     let files;
-    console.log(e.target, "target");
-
     if (e.dataTransfer) {
       files = e.dataTransfer.files;
-    }
-    if (e.target) {
+    } else if (e.target) {
       files = e.target.files;
     }
     console.log(files, "files");
@@ -37,14 +98,32 @@ const Profile = () => {
       setImage(reader.result);
     };
     reader.readAsDataURL(files[0]);
-    console.log("Hin thi is console");
+    console.log(image);
+  };
+
+  const coverUpload = (e) => {
+    e.preventDefault();
+    let files;
+    if (e.dataTransfer) {
+      files = e.dataTransfer.files;
+    } else if (e.target) {
+      files = e.target.files;
+    }
+    console.log(files, "files");
+    console.log(files[0]);
+    const reader = new FileReader();
+    reader.onload = () => {
+      setCover(reader.result);
+    };
+    reader.readAsDataURL(files[0]);
+    console.log(image);
   };
 
   return (
     <>
       <div className="bg-primary h-screen w-fll">
         <Navbar page="Profile" />
-        <div className=" m-auto w-[980px] my-8">
+        <div className=" m-auto w-[980px] my-2 h-screen">
           {profileModal && (
             <>
               <div className="w-[600px] z-10 h-[500px] border border-gray-600 py-4 px-8 rounded-xl bg-primary absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
@@ -52,7 +131,26 @@ const Profile = () => {
                   Change profile picture
                 </div>
 
-                <div>
+                <div className="my-2 flex items-center justify-center">
+                  <Cropper
+                    ref={cropperRef}
+                    style={{ height: 400, width: "100%" }}
+                    zoomTo={0.5}
+                    initialAspectRatio={1}
+                    preview=".img-preview"
+                    src={image}
+                    viewMode={1}
+                    minCropBoxHeight={10}
+                    minCropBoxWidth={10}
+                    background={false}
+                    responsive={true}
+                    autoCropArea={1}
+                    checkOrientation={false} // https://github.com/fengyuanchen/cropperjs/issues/671
+                    guides={true}
+                  />
+                </div>
+
+                <div className="flex justify-center items-center my-2">
                   <input
                     onChange={imageUpload}
                     className="text-gray-500 bg-red-500"
@@ -62,7 +160,7 @@ const Profile = () => {
 
                 <div className="flex justify-center gap-4 mt-4">
                   <button
-                    onClick={() => console.log("This is a button")}
+                    onClick={getCropData}
                     className="text-xl bg-gray-600 rounded-md py-1 px-3 text-white"
                   >
                     Done
@@ -77,21 +175,87 @@ const Profile = () => {
               </div>
             </>
           )}
-          <div className="flex justify-start gap-4 items-center w-[980px]">
-            <div className="relative group">
-              <img
-                className="w-20 h-20 rounded-full"
-                src="https://cdn-icons-png.flaticon.com/512/147/147144.png"
-                alt=""
-              />
-              <div className="bg-[#0000005d] group-hover:w-full rounded-full h-20 top-0 left-0 absolute flex justify-center items-center ">
-                <BiUpload
-                  onClick={() => setProfileModal(true)}
-                  className="group-hover:block hidden text-3xl text-white"
+          {coverModal && (
+            <>
+              <div className="w-[600px] z-10 h-[500px] border border-gray-600 py-4 px-8 rounded-xl bg-primary absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
+                <div className="text-3xl text-white text-center w-full">
+                  Change profile picture
+                </div>
+
+                <div className="my-2 flex items-center justify-center">
+                  <Cropper
+                    ref={cropperRef}
+                    style={{ height: 400, width: "100%" }}
+                    zoomTo={0.5}
+                    initialAspectRatio={1}
+                    preview=".img-preview"
+                    src={cover}
+                    viewMode={1}
+                    minCropBoxHeight={10}
+                    minCropBoxWidth={10}
+                    background={false}
+                    responsive={true}
+                    autoCropArea={1}
+                    checkOrientation={false} // https://github.com/fengyuanchen/cropperjs/issues/671
+                    guides={true}
+                  />
+                </div>
+
+                <div className="flex justify-center items-center my-2">
+                  <input
+                    onChange={coverUpload}
+                    className="text-gray-500 bg-red-500"
+                    type="file"
+                  />
+                </div>
+
+                <div className="flex justify-center gap-4 mt-4">
+                  <button
+                    onClick={getCoverData}
+                    className="text-xl bg-gray-600 rounded-md py-1 px-3 text-white"
+                  >
+                    Done
+                  </button>
+                  <button
+                    onClick={() => setCoverModal(false)}
+                    className="text-xl bg-gray-600 rounded-md py-1 px-3 text-white"
+                  >
+                    Cancel
+                  </button>
+                </div>
+              </div>
+            </>
+          )}
+          <div className="flex flex-col justify-center gap-4 items-center w-[980px]">
+            <div className="relative">
+              <div className="group">
+                <img
+                  className="w-full h-40"
+                  src={coverPhoto}
+                  alt="Cover-Photo"
                 />
+                <div className="bg-[#0000005d] group-hover:w-12 rounded-full h-12 right-0 bottom-0 absolute flex justify-center items-center ">
+                  <BiUpload
+                    onClick={() => setProfileModal(true)}
+                    className="group-hover:block hidden text-3xl text-white"
+                  />
+                </div>
+              </div>
+              <div className="absolute top-28 left-0  group">
+                <img
+                  className="w-20 h-20 rounded-full"
+                  src={data.photoURL}
+                  alt="Profile-Photo"
+                />
+                <div className="bg-[#0000005d] group-hover:w-20 rounded-full h-20 top-0 left-0 absolute flex justify-center items-center ">
+                  <BiUpload
+                    onClick={() => setProfileModal(true)}
+                    className="group-hover:block hidden text-3xl text-white"
+                  />
+                </div>
               </div>
             </div>
-            <div>
+            <div className="mt-4">
               <div className="text-3xl my-1 font-semibold text-white">
                 {data.displayName}
               </div>
@@ -101,10 +265,10 @@ const Profile = () => {
             </div>
           </div>
           <div className="text-xl text-white flex flex-col  my-8">
-            <p className="font-semibold border-b border-b-gray-500 py-3">
+            {/*   <p className="font-semibold border-b border-b-gray-500 py-3">
               About Me
-            </p>
-            <SideList title="Username" description="Bipul Hajong" />
+            </p> */}
+            {/* <SideList title="Username" description="Bipul Hajong" /> */}
             <SideList title="Email" />
             <SideList title="Phone" />
             <SideList title="Address" />
